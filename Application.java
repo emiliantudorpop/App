6b76df96783f351c0eaeef058fefc5ff6b661d48
Emilian Tudor Pop <emiliantudorpop@gmail.com>
2023-10-28 19:58:37 +0300

    Created feature 2

--- Application.java
+++ Application.java
@@ -1,5 +1,11 @@
 public class Application {
 	public static void main(String[] args) {
 		System.out.println("Welcome!");
+		presentDeveloperTwo();
 	}
+
+	public static void presentDeveloperTwo() {
+		System.out.println("Dev Two is awesome!");
+	}	
 }	
+
